b37cd39f383a1c4d699ee5613c7394779700d485
Leo Ayala <leo_a1016@yahoo.com>
2020-10-28 12:09:59 -0400

    added Cart routes and allowed for adding/removing

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,28 +1,57 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery } from "@apollo/react-hooks";
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
 import { QUERY_PRODUCTS } from "../utils/queries";
-import spinner from '../assets/spinner.gif'
+import spinner from "../assets/spinner.gif";
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from "../utils/actions";
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
-  
-  const [currentProduct, setCurrentProduct] = useState({})
-  
+
+  const [currentProduct, setCurrentProduct] = useState({});
+
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-  
-  const { products } = state;
-  
+
+  const { products, cart } = state;
+
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 },
+      });
+    }
+  };
+
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id,
+    });
+  };
+
   useEffect(() => {
     if (products.length) {
-      setCurrentProduct(products.find(product => product._id === id));
+      setCurrentProduct(products.find((product) => product._id === id));
     } else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: data.products
+        products: data.products,
       });
     }
   }, [products, data, dispatch, id]);
@@ -31,24 +60,20 @@ function Detail() {
     <>
       {currentProduct ? (
         <div className="container my-1">
-          <Link to="/">
-            ← Back to Products
-          </Link>
+          <Link to="/">← Back to Products</Link>
 
           <h2>{currentProduct.name}</h2>
 
-          <p>
-            {currentProduct.description}
-          </p>
+          <p>{currentProduct.description}</p>
 
           <p>
-            <strong>Price:</strong>
-            ${currentProduct.price}
-            {" "}
-            <button>
+            <strong>Price:</strong>${currentProduct.price}{" "}
+            <button onClick={addToCart}>
               Add to Cart
             </button>
-            <button>
+            <button
+              disabled={!cart.find((p) => p._id === currentProduct._id)}
+              onClick={removeFromCart}>
               Remove from Cart
             </button>
           </p>
@@ -59,11 +84,9 @@ function Detail() {
           />
         </div>
       ) : null}
-      {
-        loading ? <img src={spinner} alt="loading" /> : null
-      }
+      {loading ? <img src={spinner} alt="loading" /> : null}
     </>
   );
-};
+}
 
 export default Detail;
